3f42842b5dada5a0eccb16f898357ae5d20e3088
username <filipioan_29@yahoo.ro>
2026-05-08 14:39:58 +0300

    Initial commit

---
+++ src/main/java/com/example/sb_ecom_v1/SbEcomV1Application.java
@@ -0,0 +1,13 @@
+package com.example.sb_ecom_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcomV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sb_ecom_v1/SbEcomV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_ecom_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
